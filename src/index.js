b9ed6f117199b270e36d78752dc99f9940e2292e
github username <meliajm@gmail.com>
2020-03-26 09:55:52 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("#new-task-description").addEventListener("submit", function(event) {
+    event.preventDefault();
+  }, false);
 });
+
+
